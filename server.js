724b1ade9426805d6f9257e073a7e95b493d327e
hihello <13111181+hihellos@users.noreply.github.com>
2020-09-12 13:54:59 -0400

    deploying

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ app.use(express.static("public")); // Finds index.html in public
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
